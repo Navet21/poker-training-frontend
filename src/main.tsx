b59f61c4d032856c8b4e70d1091c294ce1006059
navet21 <navet21dev@gmail.com>
2026-04-20 10:15:38 +0200

    Prueba redeploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from "./App.tsx";
 import "./App.css";
 import { CardSkinProvider } from "./poker/theme/CardSkinProvider";
 
+console.log("Running in dev mode");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CardSkinProvider>
